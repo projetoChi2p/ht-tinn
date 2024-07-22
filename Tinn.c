f8d06f8e08986e98171a6a26e28200e8df616bdd
kerszamaral <kerszamaral@gmail.com>
2024-07-22 16:56:10 -0300

    Made tinn.c more generic

--- Tinn.c
+++ Tinn.c
@@ -1,9 +1,49 @@
 #include "Tinn.h"
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef TINN_EXP
 #include <math.h>
+#define TINN_EXP(x) expf(x)
+#endif
+
+#ifndef TINN_RAND
+#include <stdlib.h>
+#define TINN_RAND() rand()
+#endif
+
+#ifndef TINN_FREE
+#include <stdio.h>
+#define TINN_FREE(x) free(x)
+#endif
+
+#ifndef TINN_CALLOC
+#include <stdio.h>
+#define TINN_CALLOC(n, s) calloc(n, s)
+#endif
+
+#ifndef TINN_PRINT
+#include <stdio.h>
+#define TINN_PRINT printf
+#endif
+
+#ifndef TINN_FOPEN
+#include <stdio.h>
+#define TINN_FOPEN(path, mode) fopen(path, mode)
+#endif
+
+#ifndef TINN_FCLOSE
+#include <stdio.h>
+#define TINN_FCLOSE(file) fclose(file)
+#endif
+
+#ifndef TINN_FSCANF
+#include <stdio.h>
+#define TINN_FSCANF fscanf
+#endif
+
+#ifndef TINN_FPRINTF
+#include <stdio.h>
+#define TINN_FPRINTF fprintf
+#endif
 
 // Computes error.
 static float err(const float a, const float b)
@@ -29,7 +69,7 @@ static float toterr(const float* const tg, const float* const o, const int size)
 // Activation function.
 static float act(const float a)
 {
-    return 1.0f / (1.0f + expf(-a));
+    return 1.0f / (1.0f + TINN_EXP(-a));
 }
 
 // Returns partial derivative of activation function.
@@ -41,7 +81,7 @@ static float pdact(const float a)
 // Returns floating point random from 0.0 - 1.0.
 static float frand()
 {
-    return rand() / (float) RAND_MAX;
+    return TINN_RAND() / (float) RAND_MAX;
 }
 
 // Performs back propagation.
@@ -115,11 +155,11 @@ Tinn xtbuild(const int nips, const int nhid, const int nops)
     // Tinn only supports one hidden layer so there are two biases.
     t.nb = 2;
     t.nw = nhid * (nips + nops);
-    t.w = (float*) calloc(t.nw, sizeof(*t.w));
+    t.w = (float*) TINN_CALLOC(t.nw, sizeof(*t.w));
     t.x = t.w + nhid * nips;
-    t.b = (float*) calloc(t.nb, sizeof(*t.b));
-    t.h = (float*) calloc(nhid, sizeof(*t.h));
-    t.o = (float*) calloc(nops, sizeof(*t.o));
+    t.b = (float*) TINN_CALLOC(t.nb, sizeof(*t.b));
+    t.h = (float*) TINN_CALLOC(nhid, sizeof(*t.h));
+    t.o = (float*) TINN_CALLOC(nops, sizeof(*t.o));
     t.nips = nips;
     t.nhid = nhid;
     t.nops = nops;
@@ -130,46 +170,46 @@ Tinn xtbuild(const int nips, const int nhid, const int nops)
 // Saves a tinn to disk.
 void xtsave(const Tinn t, const char* const path)
 {
-    FILE* const file = fopen(path, "w");
+    FILE* const file = TINN_FOPEN(path, "w");
     // Save header.
-    fprintf(file, "%d %d %d\n", t.nips, t.nhid, t.nops);
+    TINN_FPRINTF(file, "%d %d %d\n", t.nips, t.nhid, t.nops);
     // Save biases and weights.
-    for(int i = 0; i < t.nb; i++) fprintf(file, "%f\n", (double) t.b[i]);
-    for(int i = 0; i < t.nw; i++) fprintf(file, "%f\n", (double) t.w[i]);
-    fclose(file);
+    for(int i = 0; i < t.nb; i++) TINN_FPRINTF(file, "%f\n", (double) t.b[i]);
+    for(int i = 0; i < t.nw; i++) TINN_FPRINTF(file, "%f\n", (double) t.w[i]);
+    TINN_FCLOSE(file);
 }
 
 // Loads a tinn from disk.
 Tinn xtload(const char* const path)
 {
-    FILE* const file = fopen(path, "r");
+    FILE* const file = TINN_FOPEN(path, "r");
     int nips = 0;
     int nhid = 0;
     int nops = 0;
     // Load header.
-    fscanf(file, "%d %d %d\n", &nips, &nhid, &nops);
+    TINN_FSCANF(file, "%d %d %d\n", &nips, &nhid, &nops);
     // Build a new tinn.
     const Tinn t = xtbuild(nips, nhid, nops);
     // Load bias and weights.
-    for(int i = 0; i < t.nb; i++) fscanf(file, "%f\n", &t.b[i]);
-    for(int i = 0; i < t.nw; i++) fscanf(file, "%f\n", &t.w[i]);
-    fclose(file);
+    for(int i = 0; i < t.nb; i++) TINN_FSCANF(file, "%f\n", &t.b[i]);
+    for(int i = 0; i < t.nw; i++) TINN_FSCANF(file, "%f\n", &t.w[i]);
+    TINN_FCLOSE(file);
     return t;
 }
 
 // Frees object from heap.
 void xtfree(const Tinn t)
 {
-    free(t.w);
-    free(t.b);
-    free(t.h);
-    free(t.o);
+    TINN_FREE(t.w);
+    TINN_FREE(t.b);
+    TINN_FREE(t.h);
+    TINN_FREE(t.o);
 }
 
 // Prints an array of floats. Useful for printing predictions.
 void xtprint(const float* arr, const int size)
 {
     for(int i = 0; i < size; i++)
-        printf("%f ", (double) arr[i]);
-    printf("\n");
+        TINN_PRINT("%f ", (double) arr[i]);
+    TINN_PRINT("\n");
 }
